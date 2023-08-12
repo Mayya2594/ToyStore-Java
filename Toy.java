ff645c839584347de017d99625b28f466e5fdf65
Mayya <majko2594@gmail.com>
2023-08-12 21:16:41 +0300

    add class Toy

---
+++ Toy.java
@@ -0,0 +1,28 @@
+class Toy implements Comparable<Toy> {
+    private String id;
+    private String name;
+    private int weight;
+
+    public Toy(String id, String name, int weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public int compareTo(Toy o) {
+        return this.weight - o.weight;
+    }
+}
